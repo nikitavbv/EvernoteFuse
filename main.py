18705f24db7e2978542e58bb79601c20589b8831
Nikita Volobuev <nikitavbv@gmail.com>
2019-01-30 15:42:15 +0200

    feat(main): create mount point if it does not exist

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import fusepass
 import logging
+import os
+import subprocess
+import getpass
 
-mountpoint = '/mnt/evernote'
+mount_point = '/mnt/evernote'
+
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-    passthrough = fusepass.Memory()
-    fusepass.FUSE(passthrough, mountpoint, foreground=True, allow_other=True)
+    if not mount_point_exists():
+        mount_point_create()
+    fusepass.Memory(mount_point)
+
+
+def mount_point_exists():
+    return os.path.isdir(mount_point)
+
+
+def mount_point_create():
+    print('Creating mount point requires root')
+    subprocess.call(['sudo', 'mkdir', mount_point])
+    subprocess.call(['sudo', 'chown', getpass.getuser(), mount_point])
+    print('Mount point created:', mount_point)
+
 
 if __name__ == '__main__':
     main()
